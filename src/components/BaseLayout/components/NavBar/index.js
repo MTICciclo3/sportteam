e22b479a338526c0dec5f8bc8a112d0bdc158e54
darwinquinte <darwinquintero@hotmail.com>
2021-10-01 23:22:32 -0500

    modificacion

--- src/components/BaseLayout/components/NavBar/index.js
+++ src/components/BaseLayout/components/NavBar/index.js
@@ -16,6 +16,9 @@ const Navbar = () => (
         <li><NavLink activeClassName="selected" className="nav-link" to="/children">Niños</NavLink></li>
       </ul>
     </div>
+    <div> 
+    <NavLink to="/login"><Icon medium>login</Icon></NavLink>
+    </div>
     <div className="shopping-cart">
       <NavLink to="/cart"><Icon medium>shopping_cart</Icon></NavLink>
     </div>
