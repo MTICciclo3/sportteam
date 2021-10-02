916eb111fc7d208e8c13eae1993c7827f1c1a8ee
jorgeivanrua <karito45@gmail.com>
2021-10-01 23:57:49 -0500

    cambio

--- src/components/Data/index.js
+++ src/components/Data/index.js
@@ -3,47 +3,15 @@ const products = [
     id: 1,
     name: "",
     description: "",
-<<<<<<< HEAD
-=======
-    price: 10,
-    gender: "men",
-    type: "",
-    img: "",
-    inCart: false,
-    category: "men"
-  },
-  
-  {
-    id: 1,
-    name: "",
-    description: "",
->>>>>>> main
     price: 0,
     gender: "men",
     type: "",
     img: "",
     inCart: false,
-<<<<<<< HEAD
     category: ""
   },
   {
     id: 2,
-=======
-    category: "men"
-  }, 
-  {
-    id: 1,
-    name: "",
-    description: "",
-    price: 0,
-    gender: "men",
-    type: "",
-    img: "",
-    inCart: false,
-    category: "men"
-  },  
-  {
-    id: 1,
     name: "",
     description: "",
     price: 0,
@@ -51,40 +19,10 @@ const products = [
     type: "",
     img: "",
     inCart: false,
-    category: "women"
-  },
-  {
-    id: 1,
->>>>>>> main
-    name: "",
-    description: "",
-    price: 0,
-    gender: "women",
-    type: "",
-    img: "",
-    inCart: false,
-<<<<<<< HEAD
     category: ""
   },
   {
     id: 3,
-=======
-    category: "women"
-  },
-  {
-    id: 1,
-    name: "",
-    description: "",
-    price: 0,
-    gender: "women",
-    type: "",
-    img: "",
-    inCart: false,
-    category: "women"
-  },
-  {
-    id: 1,
->>>>>>> main
     name: "",
     description: "",
     price: 0,
@@ -92,36 +30,9 @@ const products = [
     type: "",
     img: "",
     inCart: false,
-<<<<<<< HEAD
     category: ""
   },
   
-=======
-    category: "children"
-  },
-  {
-    id: 1,
-    name: "",
-    description: "",
-    price: 0,
-    gender: "children",
-    type: "",
-    img: "",
-    inCart: false,
-    category: "children"
-  },
-  {
-    id: 1,
-    name: "",
-    description: "",
-    price: 0,
-    gender: "children",
-    type: "",
-    img: "",
-    inCart: false,
-    category: "children"
-  },
->>>>>>> main
 ];
 
 export default products;
