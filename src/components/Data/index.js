9da18a21f086cab8cc07c60ad6ef2c9714ff3902
jorgeivanrua <karito45@gmail.com>
2021-10-01 23:53:21 -0500

    modificacion
    modificacion

---
+++ src/components/Data/index.js
@@ -0,0 +1,38 @@
+const products = [
+  {
+    id: 1,
+    name: "",
+    description: "",
+    price: 0,
+    gender: "men",
+    type: "",
+    img: "",
+    inCart: false,
+    category: ""
+  },
+  {
+    id: 2,
+    name: "",
+    description: "",
+    price: 0,
+    gender: "women",
+    type: "",
+    img: "",
+    inCart: false,
+    category: ""
+  },
+  {
+    id: 3,
+    name: "",
+    description: "",
+    price: 0,
+    gender: "children",
+    type: "",
+    img: "",
+    inCart: false,
+    category: ""
+  },
+  
+];
+
+export default products;
